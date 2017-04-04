a4ea7ec2afadff690dd653bb43dd12cd7d409ff7
TinTanMan <matthew.disalvo@kearsarge.org>
2017-04-04 11:54:49 -0400

    Added the images for all players with weapon 1

--- Player.py
+++ Player.py
@@ -3,10 +3,10 @@ import pygame, sys, math
 class Player():
     def __init__(self, pos=[0,0], tileSize= 50):
         load = pygame.image.load
-        self.imageUp = load("Resources/")
-        self.imageDown = load("")
-        self.imageleft = load("")
-        self.imageRight = load("")
+        self.imageUp = load("Resources/PlayerImages/WithOutWeapon/PlayerUpNorm.png")
+        self.imageDown = load("Resources/PlayerImages/WithOutWeapon/PlayerDownNorm.png")   #The player loads with weapon
+        self.imageleft = load("Resources/PlayerImages/WithOutWeapon/PlayerLeftNorm.png")
+        self.imageRight = load("Resources/PlayerImages/WithOutWeapon/PlayerRightNorm.png")
         
         
         #           -- Make sure to add sprite.collide with the enemy, boss and wall. 
